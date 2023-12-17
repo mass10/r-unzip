356cf20eca866b6c7efe7ffd1fe8c3e6bb6714b0
Irisawa Masaru <irisawa.masaru@gmail.com>
2023-12-17 16:42:02 +0900

    Update mod.rs

--- src/bin/runzip/application/mod.rs
+++ src/bin/runzip/application/mod.rs
@@ -9,12 +9,12 @@ impl Unzipper {
 		if !path.ends_with(".zip") {
 			return Err("Not a zip file.".into());
 		}
-		
-        let path = std::path::Path::new(path);
-        let file_stem = path.file_stem().unwrap().to_str().unwrap();
+
+		let path = std::path::Path::new(path);
+		let file_stem = path.file_stem().unwrap().to_str().unwrap();
 		// let filename = path.file_name().unwrap().to_str().unwrap();
 
-        eprintln!("file_stem: {}", file_stem);
+		eprintln!("file_stem: {}", file_stem);
 
 		let mut unzip = zip::ZipArchive::new(std::fs::File::open(path)?)?;
 		unzip.extract(file_stem)?;
